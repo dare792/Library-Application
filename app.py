1f6fc4a03c1f19a12b5237d2f4b6a9f22e0ecf55
dare792 <26570@burnside.school.nz>
2026-05-11 10:51:05 +1200

    Implement user authentication with signup and login functionality in python, add dashboard and logout routes

--- app.py
+++ app.py
@@ -20,7 +20,9 @@ def close_connection(exception):
         db.close()
 
 def query_db(query, args=(), one=False):
+    db = get_db
     cur = get_db().execute(query, args)
+    db.commit()
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
@@ -28,6 +30,9 @@ def query_db(query, args=(), one=False):
 @app.route("/")
 
 def home():
+    if 'user_id' in session:
+        return redirect('/dashboard')
+        
     return render_template('home.html')
 
 
@@ -119,7 +124,7 @@ def search():
     return render_template("search.html", results=results, letters=letters, term=term)
 
 
-#login route
+#signup route
 @app.route("/signup", methods=['GET', 'POST'])
 
 def signup():
@@ -132,7 +137,53 @@ def signup():
         last_name = request.form['last_name']
 
         try:
-            query_db('''INSET INTO''')
+            query_db('''INSERT INTO users (email, username, password, first_name, last_name) 
+                        VALUES (?, ?, ?, ?, ?)''',
+                    (email, username, password, first_name, last_name))
+            return redirect('/login')
+        
+        except sqlite3.IntegrityError:
+            flash('Email or Username already exists!')
+    
+    return render_template('signup.html')
+
+
+#login route
+@app.route("/login", methods=['GET', 'POST'])
+
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = query_db('SELECT * FROM users WHERE email = ?', one=True)
+
+        if user and check_password_hash(user['password'], password):
+            session['user_id'] = user['id']
+            session['first_name'] = user['first_name']
+            return redirect('/dashboard')
+        
+        flash('Invalid credentials')
+    
+    return render_template('login.html')
+
+#dashboard route
+#Tha dashboard is an extended home route, after successful login
+@app.route("/dashboard")
+def dashboard():
+    if 'user_id' not in session:
+        return redirect('/login')
+    
+    return f"Welcome, {session['first_name']}!"
+
+
+#logout route
+#not linked to an html
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/login')
+            
 
 
 if __name__ == '__main__':
